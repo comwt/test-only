32d1eb53312edb76fb17d856b0d71be5ab4f6450
Justin Francis <justin.francis.4@gmail.com>
2021-02-04 23:56:19 -0600

    Fix/post main release (#9)
    * fix: post-main-release

--- scripts/post-main-release.sh
+++ scripts/post-main-release.sh
@@ -1,5 +1,7 @@
 echo "****************** Updating Next *********************"
-git fetch --all
-git checkout next
-git merge main
+git fetch --all && \
+git checkout next && \
+git merge main && \
+exit 0 # the command chain was successful
 echo "****************** Updated Next *********************"
+exit 1 #if we get here one of the commands above failed
