a5e6932d519b5a7562f1baa6e555c6cb94199ff0
Justin Francis <justin.francis.4@gmail.com>
2021-02-05 00:28:06 -0600

    fix: post-main-release-3

--- scripts/post-main-release.sh
+++ scripts/post-main-release.sh
@@ -2,7 +2,8 @@ echo "****************** Updating Next ********************"
 git fetch --all && \
 git checkout next && \
 git merge main && \
+git push origin next && \
 echo "****************** Updated Next *********************" && \
 exit 0 # the command chain was successful
-echo "************* Update of Next Failed *****************"
+echo "*********** ERROR: Update of Next Failed ************"
 exit 1 #if we get here one of the commands above failed
