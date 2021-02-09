aac4ea78b38d7f8830f53be07154e5d63f13c17a
Justin Francis <justin.francis.4@gmail.com>
2021-02-09 16:59:23 -0600

    Feature/test main (#25)
    * feature: new 'multiply' function
    
    * feat: new 'multiply' function bug fix
    
    Co-authored-by: Justin Francis <justin.francis@walmart.com>

--- lib.test.js
+++ lib.test.js
@@ -1,5 +1,6 @@
 import {
     add,
+    multiply,
     subtract
 } from './lib';
 
@@ -9,6 +10,14 @@ describe("All 'lib' tests", () => {
             const val = add(10, 2);
             expect(val).toBe(12);
         });
+    });
+    describe("multiply function should:", () => {
+        test("properly multiply numbers", () => {
+            const val = multiply(10, 2);
+            expect(val).toBe(20);
+        });
+    });
+    describe("subtract function should:", () => {
         test("properly subtract numbers", () => {
             const val = subtract(10, 2);
             expect(val).toBe(8);
