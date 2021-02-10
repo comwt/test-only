8f3aa98fb026c05ff6b91b8f2c9f0437c7c4eb11
Justin Francis <justin.francis.4@gmail.com>
2021-02-10 12:50:50 -0600

    feat: updated lib (#29)
    Co-authored-by: Justin Francis <justin.francis@walmart.com>

--- lib.js
+++ lib.js
@@ -6,6 +6,10 @@ function divide (a, b) {
   return a / b;
 };
 
+function mod(a, b) {
+  return a % b;
+};
+
 function multiply(a, b) {
   return a * b;
 };
@@ -14,9 +18,14 @@ function subtract(a, b) {
   return a - b;
 };
 
+/**
+ * js doc added
+*/
+
 module.exports = {
   add: add,
   divide: divide,
+  mod: mod,
   multiply: multiply,
   subtract: subtract,
 };
